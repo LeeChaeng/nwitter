0e6e63b944df6bd397ad8feb0a0a88dd8665c884
LeeChaeng <htijl@naver.com>
2021-01-15 21:41:59 +0900

    #3.3 Realtime Nweets

--- src/components/App.js
+++ src/components/App.js
@@ -5,10 +5,12 @@ import { authService } from "fbase";
 function App() {
   const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userObj, setUserObj] = useState(null);
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if (user) {
         setIsLoggedIn(true);
+        setUserObj(user);
       } else {
         setIsLoggedIn(false);
       }
@@ -16,7 +18,7 @@ function App() {
   }, []);
   return (
     <>
-      <AppRouter isLoggedIn={isLoggedIn} />
+      <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} />
       <footer>&copy; {new Date().getFullYear()}Nwitter </footer>
     </>
   );
