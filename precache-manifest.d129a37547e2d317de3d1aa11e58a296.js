da9e0846fa896e11610892f39e32fcbbb7a7a3f7
LeeChaeng <htijl@naver.com>
2021-02-01 19:41:33 +0900

    Updates

---
+++ precache-manifest.d129a37547e2d317de3d1aa11e58a296.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2380d5fc397b85e167dd4c2555817f0c",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "3c9afb55162792fcc812",
+    "url": "/nwitter/static/css/main.84772b82.chunk.css"
+  },
+  {
+    "revision": "cfe5d4d469eb46a8f512",
+    "url": "/nwitter/static/js/2.77cc1662.chunk.js"
+  },
+  {
+    "revision": "192d8646908f6aefe7151e10acacf833",
+    "url": "/nwitter/static/js/2.77cc1662.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c9afb55162792fcc812",
+    "url": "/nwitter/static/js/main.acab7fb9.chunk.js"
+  },
+  {
+    "revision": "940c340c7d15a25fb1c2",
+    "url": "/nwitter/static/js/runtime-main.29e886cc.js"
+  }
+]);
